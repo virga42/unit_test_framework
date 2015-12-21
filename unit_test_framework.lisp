6e3395b7247e6d64e1a267b727830ebec1c4951f
virga42 <tcallaha@gmail.com>
2015-12-21 15:17:59 -0500

    added files

---
+++ unit_test_framework.lisp
@@ -0,0 +1,23 @@
+(defmacro with-gensyms ((&rest names) &body body)
+  `(let ,(loop for n in names collect `(,n (gensym)))
+  ,@body))
+
+(defmacro check (&body forms)
+  `(combine-results
+    ,@(loop for f in forms collect `(report-result ,f ',f))))
+
+(defmacro combine-results (&body forms)
+  (with-gensyms (result)
+    `(let ((,result t))
+      ,@(loop for f in forms collect `(unless ,f (setf ,result nil)))
+      ,result)))
+
+(defun report-result (result form)
+  (format t "~:[FAIL~;pass~] ... ~a~%" result form)
+  result)
+
+(defun perform-tests ()
+  (check
+    (= (+ 1 2) 4)
+    (= (+ 1 2 3) 6)
+    (= (+ -1 -3) -5)))
